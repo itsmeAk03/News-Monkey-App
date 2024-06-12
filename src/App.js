3d205a4ff9b9b4bea2a068bb4cbd2bb65b94d06b
Anupam Kumar Bhadra <anupambhadra9@gmail.com>
2024-06-13 00:17:44 +0530

    new components added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import NavBar from './Components/NavBar'
+import News from './Components/News'
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        Hello my first class based Component
+        <NavBar/>
+        <News/>
       </div>
     )
   }
