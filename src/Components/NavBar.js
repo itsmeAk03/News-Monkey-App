05769c65a2c473b4e0faaeffe399bedd8e28a95d
Anupam Kumar Bhadra <anupambhadra9@gmail.com>
2024-07-02 00:01:34 +0530

    added-badge,authour,source and date time

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -16,7 +16,7 @@ export class NavBar extends Component {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+          <Link className="nav-link active" aria-current="page" to="/general">Home</Link>
         </li>
      
 
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -2,14 +2,18 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let {title , description,imageUrl,newsUrl }=this.props
+    let {title , description,imageUrl,newsUrl,author,date,source }=this.props
     return (
       <div className='my-3'>
             <div className="card" >
+            <span class="position-absolute top-0  translate-middle  bg-danger badge rounded-pill " style={{left:'90%',zIndex:'1'}}>
+              <span class="source">{source}</span>
+            </span>
                   <img src={!imageUrl?"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2Ll11DJHBcNQO6AJnQlnAMZCeF_k1ASykIw&s":imageUrl} className="card-img-top" alt="..."/>
                   <div className="card-body">
                     <h5 className="card-title">{title}...</h5>
                     <p className="card-text">{description}...</p>
+                    <p class="card-text"><small className="text-body-secondary">By {!author?"Unknown":author} on {new Date(date).toUTCString()}</small></p>
                     <a href={newsUrl} target='blank' className="btn btn-sm btn-primary">Read More</a>
                   </div>
             </div>
